25876dd24d15906bcc9c06e0f7554f6c2b5b5ecd
ryo1999 <ryo.soccer19990416@gmail.com>
2022-10-06 21:44:02 +0900

    feat: アプリのタイトルコンポーネント作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from "next/app"
 import { RecoilRoot } from "recoil"
 import "../../styles/globals.css"
 import ThemeProvider from "@/theme"
+import Header from "@/components/molecules/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider>
         <RecoilRoot>
+            <Header/>
             <Component {...pageProps} />
         </RecoilRoot>
         </ThemeProvider>
