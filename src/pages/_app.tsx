8ffe323d771a4e50f6dd1300c475820b4bdff299
ryo1999 <ryo.soccer19990416@gmail.com>
2022-10-08 20:55:57 +0900

    refactor: Head追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import type { AppProps } from "next/app"
-import { NextSeo } from 'next-seo'
+// import { NextSeo } from 'next-seo'
 import { RecoilRoot } from "recoil"
 import "../../styles/globals.css"
 import ThemeProvider from "@/theme"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider>
             <RecoilRoot>
-                <NextSeo title="EmoQ" description="グループで気持ちと共に疑問を共有" />
+                <Head>
+                    <title>EmoQ</title>
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="description"
+                        content="グループで気持ちと共に疑問を共有"
+                    ></meta>
+                </Head>
+                {/* <NextSeo title="EmoQ" description="グループで気持ちと共に疑問を共有" /> */}
                 <Component {...pageProps} />
             </RecoilRoot>
         </ThemeProvider>
