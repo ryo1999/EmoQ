e0ab8882fbf6fdd4ba4f211721d204edc9d9b0ed
ryo1999 <ryo.soccer19990416@gmail.com>
2022-10-08 19:29:48 +0900

    refactor: head削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,11 @@ import { NextSeo } from 'next-seo'
 import { RecoilRoot } from "recoil"
 import "../../styles/globals.css"
 import ThemeProvider from "@/theme"
-import Header from "@/components/molecules/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider>
             <RecoilRoot>
-                {/* <Header /> */}
                 <NextSeo title="EmoQ" description="グループで気持ちと共に疑問を共有" />
                 <Component {...pageProps} />
             </RecoilRoot>
