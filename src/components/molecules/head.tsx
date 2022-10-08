e104cb21384d33cc9c9739a9133e6d549ee4b72f
ryo1999 <ryo.soccer19990416@gmail.com>
2022-10-08 18:56:39 +0900

    refactor: metaタグ設定

--- src/components/molecules/head.tsx
+++ src/components/molecules/head.tsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <Head>
             <title>EmoQ</title>
+            <meta name="description" content="グループで気持ちと共に疑問を共有" />
         </Head>
     )
 }
