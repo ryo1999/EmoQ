adc42a1516ecc3ba31a64a2237b2aed4dc8c6e3a
ryo1999 <ryo.soccer19990416@gmail.com>
2022-10-10 15:57:13 +0900

    refactor: meta画像変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,8 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                     <meta name="description" content="グループで気持ちと共に疑問を共有"></meta>
                     <meta property="og:title" content="EmoQ" />
                     <meta property="og:description" content="グループで気持ちと共に疑問を共有" />
-                    {/* <meta property="og:image" content="https://media.istockphoto.com/vectors/cute-emoji-looking-up-at-a-question-mark-indicating-a-problem-problem-vector-id941224134" /> */}
-                    <meta property="og:image" content="file:///Users/otsukiryou/Downloads/emoqImage.png"/>
+                    <meta property="og:image" content="https://media.istockphoto.com/vectors/cute-emoji-looking-up-at-a-question-mark-indicating-a-problem-problem-vector-id941224134" />
                     <meta name="twitter:image" content="/notmobile.png" />
                 </Head>
                 <Component {...pageProps} />
