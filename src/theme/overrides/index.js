3ee569d4dfb6c4701336c20a60335c79e679cb1c
ryo1999 <ryo.soccer19990416@gmail.com>
2022-12-07 15:32:53 +0900

    refactor: 未使用ファイル削除

--- src/theme/overrides/index.js
+++
@@ -1,9 +0,0 @@
-import Chip from "./Chip"
-import Typography from './Typography'
-
-export default function ComponentsOverrides(theme) {
-   return Object.assign(
-      Typography(theme),
-      Chip(theme),
-   )
-}
